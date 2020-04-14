ae4be349e7c7db81c533dd1fee9cf0295abf878b
TheFry <luke@archMac.localdomain>
2020-04-13 22:05:52 -0700

    Ethernet Header Parsed

--- trace.c
+++ trace.c
@@ -10,60 +10,72 @@
 #include <netinet/ether.h>
 #include <endian.h>
 
-/* Number of 16bit integers in MAC */
-#define MAC_16_SIZE 3
+/* Number of bytes for MAC address*/
+#define MAC_BYTES 6
 struct eth_frame{
-   uint16_t dst[MAC_16_SIZE];
-   uint16_t src[MAC_16_SIZE];
+   uint8_t dst[MAC_BYTES];
+   uint8_t src[MAC_BYTES];
    uint16_t type;
 } __attribute__((packed));
 
 void parse_ether();
 void print_ether(struct eth_frame);
+void read_packets();
 
+/* Globals */
+static pcap_t *file;
+static struct pcap_pkthdr *hdr;
+static const u_char *data;
 
+/* Parse args, open file, launch program */
 int main(int argc, char **argv){
-   parse_ether();
-   return 0;
-}
-
-
-void parse_ether(){
    char errbuf[PCAP_ERRBUF_SIZE];
-   struct pcap_pkthdr *hdr;
-   const u_char *data;
-   struct eth_frame frame;
-
-
    /* Open file */
-   pcap_t *file = pcap_open_offline("./given/arp/ArpTest.pcap", errbuf);
+   file = pcap_open_offline("./given/arp/ArpTest.pcap", errbuf);
 
    if(file == NULL){
       printf("%s\n", errbuf);
       exit(-1);
    }
 
-   if(pcap_next_ex(file, &hdr, &data) == PCAP_ERROR){
-      perror("Pcap read issue");
-   }
-
-   printf("Length of packet: %d\n", hdr->caplen);
-
-
-   memcpy(&frame, data, 14);
-   print_ether(frame);
+   read_packets();
+   return 0;
 }
 
 
-void print_ether(struct eth_frame frame){
-   uint16_t current = 0;
-   int i = 0;
-   /* Print Src */
-   for(i = 0; i < MAC_16_SIZE; i++){
-      memcpy(&current, frame.dst[i], sizeof(uint16_t));
-      current = ntohs(current);
-      
+/*Search Packets for headers */
+void read_packets(){
+   int retval = 0;
+   int i = 1;
+
+   retval = pcap_next_ex(file, &hdr, &data);
+   
+   while(retval == 1){
+      printf("Packet number: %d  Frame Len: %d\n\n", i, hdr->caplen);
+      parse_ether();
+      ++i;
+      retval = pcap_next_ex(file, &hdr, &data);
    }
 
-   
+   if(retval == PCAP_ERROR){
+      perror("Error reading next packet");
+      exit(-1);
+   }
+}
+
+
+void parse_ether(){
+   struct ether_addr address;
+   struct eth_frame frame;
+
+   printf("\tEthernet Header\n");
+   memcpy(&frame, data, sizeof(struct eth_frame));
+
+   /* Dest */
+   memcpy(address.ether_addr_octet, frame.dst, MAC_BYTES);
+   printf("\t\tDest MAC: %s\n", ether_ntoa(&address)); 
+
+   /* Src */
+   memcpy(address.ether_addr_octet, frame.src, MAC_BYTES);
+   printf("\t\tSource MAC: %s\n\n\n", ether_ntoa(&address)); 
 }
