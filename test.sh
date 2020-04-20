049557f8b4ae0b7f0675f69b3ee7bf7702d85bf1
TheFry <luke@archMac.localdomain>
2020-04-20 15:13:08 -0700

    Added test script

---
+++ test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+function run_test () {
+   ./trace $file > test_out
+   diff test_out $file.out > diff_out
+   
+   if ! [ -s diff_out ] 
+   then
+      printf "$file good\n"
+      cat diff_out
+   else
+      printf "Error on $file\n"
+      diff test_out $file.out > $file.diff_out
+      ./trace $file > $file.out
+   fi
+}
+
+printf "Testing Trace\n\n"
+
+#arp
+file=given/arp/ArpTest.pcap
+run_test
+
+#ip
+file=given/ip/IP_bad_checksum.pcap
+run_test
+
+#ping
+file=given/PingTest.pcap
+run_test
+
+#http
+file=given/http/Http.pcap
+run_test
+
+#tcp
+file=given/tcp/smallTCP.pcap
+run_test
+
+file=given/tcp/TCP_bad_checksum.pcap
+run_test
+
+#udp
+file=given/udp/UDPfile.pcap
+run_test
+
+#mix
+file=given/mix/largeMix.pcap
+run_test
+
+file=given/mix/largeMix2.pcap
+run_test
+
+file=given/mix/mix_withIPoptions.pcap
+run_test
+
+
+
+
+
+
